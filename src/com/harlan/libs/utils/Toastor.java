593594e158d5cb11f44a1524d427815b17b75847
Harlan <mikul@live.cn>
2015-05-31 19:17:45 +0800

    update Toastor

--- src/com/harlan/libs/utils/Toastor.java
+++ src/com/harlan/libs/utils/Toastor.java
@@ -1,9 +1,9 @@
 package com.harlan.libs.utils;
 
-import com.harlan.libs.core.Harlan;
-
 import android.content.Context;
 import android.widget.Toast;
+
+import com.harlan.libs.core.Harlan;
 
 /**
  * Toast Manager
@@ -21,35 +21,21 @@ public class Toastor {
 	/**
 	 * @param context
 	 * @param obj
-	 *            string or resId
 	 * @param duration
 	 */
 	private static void show(Context context, Object obj, int duration) {
-		if (toast == null) {
+		if (toast == null)
 			toast = Toast.makeText(context, obj.toString(), duration);
-		} else {
+		else {
 			toast.setDuration(duration);
 			toast.setText(obj.toString());
-
-		}
-		toast.show();
-	}
-
-	private static void showResString(Context context, int resId, int duration) {
-		if (toast == null) {
-			toast = Toast.makeText(context, resId, duration);
-		} else {
-			toast.setDuration(duration);
-			toast.setText(resId);
-
 		}
 		toast.show();
 	}
 
 	private static void setContext() {
-		if (context == null) {
+		if (context == null)
 			context = Harlan.getContext();
-		}
 	}
 
 	public static void showShort(Object obj) {
@@ -67,16 +53,6 @@ public class Toastor {
 		showTest(context);
 	}
 
-	public static void showShortResString(int resId) {
-		setContext();
-		showShortResString(context, resId);
-	}
-
-	public static void showLongResString(int resId) {
-		setContext();
-		showLongResString(context, resId);
-	}
-
 	public static void showShort(Context context, Object obj) {
 		show(context, obj, Toast.LENGTH_SHORT);
 	}
@@ -87,13 +63,5 @@ public class Toastor {
 
 	public static void showTest(Context context) {
 		show(context, "测试", Toast.LENGTH_SHORT);
-	}
-
-	public static void showShortResString(Context context, int resId) {
-		showResString(context, resId, Toast.LENGTH_SHORT);
-	}
-
-	public static void showLongResString(Context context, int resId) {
-		showResString(context, resId, Toast.LENGTH_LONG);
 	}
 }
